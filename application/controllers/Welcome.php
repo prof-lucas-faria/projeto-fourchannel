4a7a09c10fdb3aa5baf14630b0950486bfb188c0
LuanaQ <luanaqueiros14@gmail.com>
2019-08-21 19:52:17 -0300

    q

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,17 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
-	// function __construct() {
-	// 	parent::__construct();
-	// 	$this->load->library('TMDb');
-	// }
+	 function __construct() {
+	 	parent::__construct();
+	 	$this->load->library('TMDb');
+	 }
 	
 	public function index()
 
 	{
-		// $id = "tt0088247";
-		// $json = $this->tmdb->getMovie(420818);
-		// $data["info"] = $json;
-		$this->load->view('welcome_message');
+		 $id = "tt0088247";
+		 $json = $this->tmdb->getMovie(420818);
+		 $data["info"] = $json;
+		$this->load->view('welcome_message',$data);
 	}
 }
